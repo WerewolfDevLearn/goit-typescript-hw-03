dd915cd42315cee40cc82f6c8d550872f4d2d3d0
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-08-27 11:28:10 +0300

    first attempt

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,45 @@
+class Key {
+	private signature: number = Math.random();
+	getSignature() {
+		return this.signature;
+	}
+}
+class Person {
+	private key: Key;
+	constructor(key: Key) {
+		this.key = key;
+	}
+	getKey() {
+		return this.key;
+	}
+}
+
+abstract class House {
+	door: boolean = false;
+	protected key: Key;
+	tenants: Person[] = [];
+	openDoor(key: Key): void {
+		if (key === this.key) {
+			this.door = true;
+		}
+	}
+	comeIn(tenant: Person): void {
+		if (this.door) {
+			this.tenants.push(tenant);
+		}
+	}
+}
+
+class MyHouse extends House {
+	constructor(key: Key) {
+		super();
+		this.key = key;
+	}
+	openDoor(key: Key): void {
+		super.openDoor(key);
+	}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +48,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
